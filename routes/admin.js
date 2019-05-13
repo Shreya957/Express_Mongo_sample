b2c3a3787829519b3b27c587c4251221e1cecb08
Shreyanka <shreya957@gmail.com>
2019-05-13 22:08:30 +0530

    add routes folder for the app

---
+++ routes/admin.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express =  require('express');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product',(req, res, next) => {
+    console.log('In the middleware');
+    //res.send('<form action="/product" method="POST"><input type="text" name = "title"><button type = "submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+});
+
+router.post('/add-product',(req, res, next) => {
+    
+    console.log(req.body);
+    products.push({title: req.body.title})
+    res.redirect('/');
+    
+});
+
+exports.routes = router;
+exports.products = products;
---
+++ routes/shop.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+const adminData = require('./admin');
+
+router.get('/',(req, res, next) => {
+     console.log('shopjs',adminData.products);
+     res.sendFile(path.join(__dirname, '../', 'views','shop.html'))
+
+});
+
+module.exports = router;
