bda4ef418d408b199a693ef2b18f716742069617
Shreyanka <shreya957@gmail.com>
2019-05-16 16:45:59 +0530

    adding modifies routes

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,8 @@ const products = [];
 router.get('/add-product',(req, res, next) => {
     console.log('In the middleware');
     //res.send('<form action="/product" method="POST"><input type="text" name = "title"><button type = "submit">Add Product</button></form>');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    //res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.render('add-product.pug', {pageTitle: 'Add Product'})
 });
 
 router.post('/add-product',(req, res, next) => {
